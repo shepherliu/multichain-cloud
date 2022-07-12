469182aff2a5788855bada908d1f8eb67f6aacca
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-07-12 10:41:52 +0800

    add sleep for handleClick

--- src/pages/DaosPage.vue
+++ src/pages/DaosPage.vue
@@ -440,6 +440,8 @@ const getVoteCount = async (votetype:string) => {
 
 //click to change active item and refresh page
 const handleClick = async () => {
+  //wait for the active name change
+  await tools.sleep(100);  
 
   connectState.activeName.value = activeName.value;
   tools.setUrlParamter('activeName', activeName.value);
--- src/pages/FoldersPage.vue
+++ src/pages/FoldersPage.vue
@@ -161,6 +161,8 @@ const getFileCount = async (filetype:string) => {
 
 //click to change active item and refresh page
 const handleClick = async () => {
+  //wait for the active name change
+  await tools.sleep(100);  
   
   connectState.activeName.value = activeName.value;
   tools.setUrlParamter('activeName', activeName.value);
